b7c9d0dcdd90698c65e693f5deb3718d9e10a1d2
DuocCosas <jor.videla@profesor.duoc.cl>
2025-04-22 20:00:31 -0400

    cambio de mensaje

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Prendio la app! Wena");
+		System.out.println("App prendida");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
